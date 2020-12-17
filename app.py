52d1b36cbf2e05279103ea17cbdc6438549cba8f
KushGabani <kushgabz2687@gmail.com>
2020-12-17 14:16:46 +0530

    character-recognition (working)

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 import numpy as np
 import cv2
-from keras.models import load_model
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 from flask import Flask, url_for, request, render_template
 from werkzeug.utils import secure_filename
 
